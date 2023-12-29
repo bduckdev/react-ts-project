d40203daca28f0cc7ef787079f430af6634fe9bd
Drew Pereli <drewpereli@gmail.com>
2023-12-28 17:31:51 -0800

    tmp

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,19 @@
 import Header from './Header';
 import './App.css';
 import ImageCard, { Image } from './ImageCard';
-import Gallery from './Gallery';
+// import Gallery from './Gallery';
 import { useEffect, useState } from 'react';
 import images from './images.json';
+import Gallery from './Gallery';
 
 function App() {
-  const [users, setUsers] = useState<{ name: string }[] | null>(null);
-
-  useEffect(() => {
-    loadData().then((users) => {
-      setUsers(users);
-    });
-  }, []);
-
-  if (users === null) {
-    return <div>Loading....</div>;
-  }
+  const [images, setImages] = useState<Image[]>([]);
 
   return (
-    <div>
-      <span>{users[0].name}</span>
-    </div>
+    <main>
+      <Gallery images={images} />
+    </main>
   );
-}
-
-async function loadData() {
-  console.log('loading data');
-  // fetch users
-  await new Promise((res) => setTimeout(res, 1000));
-
-  return [
-    {
-      name: 'Jim',
-    },
-    {
-      name: 'Bob',
-    },
-  ];
 }
 
 export default App;
